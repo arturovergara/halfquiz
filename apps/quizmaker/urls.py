bb0c982be0d5ceb198eadde4b131b962b1f9d2fe
Xtrem <arturovergara@protonmail.com>
2023-02-05 18:19:52 -0300

    feat(): Add topic list view

--- apps/quizmaker/urls.py
+++ apps/quizmaker/urls.py
@@ -1,7 +1,10 @@
 # Django Imports
 from django.urls import path
 
-from .views import TestView
+from .views import TestView, TopicListView
 
 app_name = "quizmaker"
-urlpatterns = [path("test/", TestView.as_view(), name="test")]
+urlpatterns = [
+    path("test/", TestView.as_view(), name="test"),
+    path("topics/", TopicListView.as_view(), name="topic_list"),
+]
--- apps/quizmaker/views.py
+++ apps/quizmaker/views.py
@@ -1,6 +1,13 @@
 # Django Imports
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
+
+from .models import Topic
 
 
 class TestView(TemplateView):
     template_name = "quizmaker/example.html"
+
+
+class TopicListView(ListView):
+    model = Topic
+    context_object_name = "topics"
