1b6fafb5fe80cdda22083b9fe68304767d04be52
Xtrem <arturovergara@protonmail.com>
2023-02-24 18:20:00 -0300

    feat(quizmaker): Add TopicDeleteView

--- apps/quizmaker/urls.py
+++ apps/quizmaker/urls.py
@@ -2,10 +2,12 @@
 from django.urls import path, reverse_lazy
 from django.views.generic import RedirectView
 
-from .views import TopicListView
+from .views import TopicCreateView, TopicDeleteView, TopicListView
 
 app_name = "quizmaker"
 urlpatterns = [
+    path("topics/<int:pk>/delete/", TopicDeleteView.as_view(), name="topic_delete"),
+    path("topics/add/", TopicCreateView.as_view(), name="topic_create"),
     path("topics/", TopicListView.as_view(), name="topic_list"),
     path(
         "", RedirectView.as_view(url=reverse_lazy("quizmaker:topic_list")), name="home"
--- apps/quizmaker/views.py
+++ apps/quizmaker/views.py
@@ -1,6 +1,8 @@
 # Django Imports
+from django.contrib.messages.views import SuccessMessageMixin
+from django.http.response import Http404
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, DeleteView, ListView
 
 from .forms import TopicForm
 from .models import Topic
@@ -11,8 +13,17 @@ class TopicListView(ListView):
     context_object_name = "topics"
 
 
-class TopicCreateView(CreateView):
+class TopicCreateView(SuccessMessageMixin, CreateView):
     model = Topic
     form_class = TopicForm
     success_url = reverse_lazy("quizmaker:topic_list")
     success_message = "Topic was created successfully!"
+
+
+class TopicDeleteView(SuccessMessageMixin, DeleteView):
+    model = Topic
+    success_url = reverse_lazy("quizmaker:topic_list")
+    success_message = "Topic was deleted successfully!"
+
+    def get(self, request, *args, **kwargs):
+        raise Http404("Only POST method available")
