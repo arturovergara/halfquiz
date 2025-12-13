7d6b37ff52dd639b0018a444e9cf71e421b8f98f
Xtrem <arturovergara@protonmail.com>
2025-12-13 20:50:28 -0300

    build: Optimize production config for small VM

--- compose/production/app/start.sh
+++ compose/production/app/start.sh
@@ -8,4 +8,4 @@ python3 /app/halfquiz/manage.py collectstatic --no-input
 python3 /app/halfquiz/manage.py migrate --no-input
 
 # Start production server
-gunicorn --bind 0.0.0.0:8000 --workers=4 config.wsgi:application
+gunicorn --bind 0.0.0.0:8000 --workers=1 --threads=2 --timeout=30 --max-requests=500 --max-requets-jitter=50 config.wsgi:application
--- config/settings/production.py
+++ config/settings/production.py
@@ -13,6 +13,7 @@ CSRF_TRUSTED_ORIGINS = [f"https://{host}" for host in ALLOWED_HOSTS]
 # Database
 DATABASE_DIR = BASE_DIR.parent / "sqlite-data"
 DATABASES["default"]["NAME"] = DATABASE_DIR / "db.sqlite3"  # noqa: F405
+CONN_MAX_AGE = 0
 
 # Admin
 ADMIN_URL = env("DJANGO_ADMIN_URL")
